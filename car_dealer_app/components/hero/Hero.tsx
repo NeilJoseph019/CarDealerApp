2f99b78a34b6627cebb55101701a081ecfd1948e
Neil Joseph <neiljoseph@Neils-MacBook-Pro.local>
2023-07-25 17:57:50 +0100

    Image added to the hero component.

--- car_dealer_app/components/hero/Hero.tsx
+++ car_dealer_app/components/hero/Hero.tsx
@@ -1,4 +1,5 @@
 import styles from './hero.module.css'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
@@ -6,6 +7,12 @@ const Hero = () => {
       <div className={styles.hero__title__container}>
         <h1 className={styles.hero__title}> Search, book, or sell cars -- quickly and easily</h1>
         <p className={styles.hero__subtitle}> Relax and Enjoy a Stellar Car Dealing Experience with Us.</p>
+      </div>
+      <div className={styles.hero__image_container}>
+          <div className={styles.hero__image}>
+            <Image src="/hero.png" alt='Hero car img' fill className='object-contain' />
+      
+          </div>
       </div>
     </div>
   )
