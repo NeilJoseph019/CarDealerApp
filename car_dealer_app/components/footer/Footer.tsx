35610364704076217739f6087967ca19fb9465c1
Neil Joseph <neiljoseph@Neils-MacBook-Pro.local>
2023-08-02 09:46:52 +0100

    Completed Footer

--- car_dealer_app/components/footer/Footer.tsx
+++ car_dealer_app/components/footer/Footer.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+import { footerLinks } from '@/constants'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='border-t-2 border-gray-200 bg-slate-100 p-2 font-paragraphs'>
+      <div className='p-3 flex justify-between'>
+
+        <div className=''>
+          <div className='flex ml-4 '>
+              <Image src={'/car-256.png'} alt='Car logo' height={55} width={55} />
+              <h1 className='mt-2 font-logo font-bold text-[1rem] md:text-[1.5rem] text-black'>CarDeal</h1>
+          </div>
+          <div className='font-paragraphs '>
+            <p> CarDeal 2023</p>
+            <p>All Rights Reserved &copy;</p>
+          </div>
+        </div>
+          
+        <div className='ml-7 grid grid-cols-2 md:grid-cols-3 gap-2 md:gap-8 '>
+          {
+            footerLinks.map(link =>(
+              <div className='flex flex-col flex-wrap'>
+                <p className='font-bold'> {link.title} </p>
+                {
+                  link.links.map(links =>(
+                    <Link href={links.url} className='hover:bg-slate-200 rounded-[1rem] px-2' >{links.title}</Link>
+                  ))
+                }
+              </div>
+            ))
+          }
+          
+        </div>
+
+      </div>
+    </footer>
   )
 }
 
--- car_dealer_app/components/navbar/Navbar.tsx
+++ car_dealer_app/components/navbar/Navbar.tsx
@@ -8,10 +8,10 @@ const Navbar = () => {
       <div className={styles.nav}>
         <div className='flex ml-4 '>
             <Image src={'/car-256.png'} alt='Car logo' height={55} width={55}/>
-            <h1 className='ml-1 mt-4'>CarDeal</h1>
+            <h1 className='mt-2 font-logo font-bold text-[1.5rem] text-white'>CarDeal</h1>
         </div>
-        <div className='mr-[5rem] mt-1'>
-          <button className='border-2 px-3 py-2 rounded-[1.5rem] hover:bg-slate-300 shadow-md'>
+        <div className='mr-[2rem] mt-1'>
+          <button className='border-2 px-3 py-2 rounded-[1.5rem] text-white hover:bg-slate-300 hover:text-black shadow-md'>
             Sign-In
           </button>
         </div>
