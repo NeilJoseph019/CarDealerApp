87d606f97afc0dd912e172e64d48c25403d68504
Neil Joseph <neiljoseph@Neils-MacBook-Pro.local>
2023-08-02 05:58:10 +0100

    Navbar completed

--- car_dealer_app/components/navbar/Navbar.tsx
+++ car_dealer_app/components/navbar/Navbar.tsx
@@ -3,15 +3,19 @@ import styles from './navbar.module.css'
 
 const Navbar = () => {
   return (
-    <nav className={styles.nav}>
-        <div>
-            <div>
-                <Image src={'/car-256.png'} alt='Car logo' height={60} width={60}/>
-            </div>
-            <div>
-                <h1>CarDeal</h1>
-            </div>
+
+    <nav className='mt-3 flex justify-center'>
+      <div className={styles.nav}>
+        <div className='flex ml-4 '>
+            <Image src={'/car-256.png'} alt='Car logo' height={55} width={55}/>
+            <h1 className='ml-1 mt-4'>CarDeal</h1>
         </div>
+        <div className='mr-[5rem] mt-1'>
+          <button className='border-2 px-3 py-2 rounded-[1.5rem] hover:bg-slate-300 shadow-md'>
+            Sign-In
+          </button>
+        </div>
+      </div>
     </nav>
   )
 }
